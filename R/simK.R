db4a29ba892268808a73140152951816cc96231d
Bruno Lima <balima78@gmail.com>
2023-04-29 17:37:49 +0100

    descr funções principais

--- R/simK.R
+++ R/simK.R
@@ -3,7 +3,7 @@
 #' The simK package provides functions for data simulation of clinical and demographic features from cadaveric donors and wait list transplant candidates.
 #'
 #' @section simK functions:
-#' The \code{simK} main functions: \code{candidates_df()}, \code{donors_df()}, \code{Abs_df()}
+#' The \code{simK} main functions: \code{candidates__df()}, \code{donors__df()}, \code{Abs__df()}
 #'
 #' @importFrom stats rnorm rpois
 #' @docType package
